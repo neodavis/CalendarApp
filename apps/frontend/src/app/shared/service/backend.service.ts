6f5459d0df4a49dc802cd72697b641ecd4bccf0d
dadadav1s <88556637+neodavis@users.noreply.github.com>
2022-12-19 22:40:48 +0200

    Update backend.service.ts

--- apps/frontend/src/app/shared/service/backend.service.ts
+++ apps/frontend/src/app/shared/service/backend.service.ts
@@ -11,18 +11,18 @@ export class BackendService {
   constructor(private http: HttpClient) { }
 
   getAbsences(): Observable<Absence[]> {
-    return this.http.get<Absence[]>('http://localhost:3333/api').pipe(delay(300))
+    return this.http.get<Absence[]>('/api').pipe(delay(300))
   }
   
   deleteAbsence(id: number): Observable<Absence[]> {
-    return this.http.post<Absence[]>('http://localhost:3333/api/delete', {id: id}).pipe(delay(300))
+    return this.http.post<Absence[]>('api/delete', {id: id}).pipe(delay(300))
   }
 
   createAbsence(absence: Absence): Observable<Absence[]> {
-    return this.http.post<Absence[]>('http://localhost:3333/api/create', {absence: absence}).pipe(delay(300))
+    return this.http.post<Absence[]>('/api/create', {absence: absence}).pipe(delay(300))
   }
   
   editAbsence(absence: Absence): Observable<Absence[]> {
-    return this.http.post<Absence[]>('http://localhost:3333/api/edit', {absence: absence}).pipe(delay(300))
+    return this.http.post<Absence[]>('/api/edit', {absence: absence}).pipe(delay(300))
   }
 }
